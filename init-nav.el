e4f3ea465a26754ca993999b12ff4e278b995757
Litong Su <loop.cm@gmail.com>
2012-08-26 19:50:20 +0800

    simple file-system navigation emacs nav

---
+++ init-nav.el
@@ -0,0 +1,9 @@
+;; emacs-nav : simple file-system navigation
+
+(require 'nav)
+(nav-disable-overeager-window-splitting)
+;; Optional: set up a quick key to toggle nav
+;; (global-set-key [f8] 'nav-toggle)
+
+
+(provide 'init-nav)
